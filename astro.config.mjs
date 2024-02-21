29797e1b6549d384e601bf2906534b8474aa4d43
energywraith <energywraith@gmail.com>
2024-02-21 20:51:37 +0100

    Add Typewriter to banner header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
+import preact from "@astrojs/preact";
 import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt(), icon()],
+  integrations: [preact(), tailwind(), sitemap(), robotsTxt(), icon()],
   site: SITE_URL,
   markdown: {
     syntaxHighlight: "shiki",
