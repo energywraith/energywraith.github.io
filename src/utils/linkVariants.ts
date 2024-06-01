f6d0ecbf23b7af7bc35d23463ad059d532897956
energywraith <energywraith@gmail.com>
2024-06-01 13:55:15 +0200

    Add hover effects

--- src/utils/linkVariants.ts
+++ src/utils/linkVariants.ts
@@ -1,6 +1,6 @@
 export const buttonVariants = {
   default: "bg-neutral-900 text-white",
-  accent: "bg-pink-600 text-white",
+  accent: "bg-pink-600 text-white hover:bg-pink-700",
 };
 
 export type ButtonVariant = keyof typeof buttonVariants;
